fa8b9f3ea4f177289b6f5525989a41d3a362cb52
Lay <jserlay@gmail.com>
2016-06-03 19:07:27 +0800

    Button Update

--- examples/src/views/ButtonView.js
+++ examples/src/views/ButtonView.js
@@ -1,9 +1,61 @@
 import React from 'react'
+import { NavBar, Container, Button } from 'layui'
+const ButtonGroup = Button.Group
+const ContainerBlock = Container.Block
 
 export default class ButtonView extends React.Component {
+  handleTap (e) {
+    e.preventDefault()
+    alert('Tapped!')
+  }
+
   render () {
     return (
-      <div></div>
+      <div>
+        <NavBar title="按钮" />
+        <Container>
+          <ContainerBlock>
+            <p><Button onTouchTap={this.handleTap.bind(this)}>Usual Button 1</Button></p>
+            <p><Button uiStyle="light">light button</Button></p>
+            <p><Button uiStyle="dark">dark button</Button></p>
+            <p><Button uiStyle="success">success button</Button></p>
+            <p><Button uiStyle="danger">danger button</Button></p>
+            <p><Button uiStyle="warning">warning button</Button></p>
+            <p><Button disabled={true}>disabled button</Button></p>
+            <p><Button uiRound={true}>Round Button 1</Button></p>
+            <p><Button uiBig={true}>Big Button</Button></p>
+            <p><Button uiBig={true} uiRound={true}>Big Round Button</Button></p>
+            <ButtonGroup>
+              <Button>Button 1</Button>
+              <Button>Button 2</Button>
+              <Button>Button 3</Button>
+            </ButtonGroup>
+            <ButtonGroup>
+              <Button active={true}>Button 1</Button>
+              <Button>Button 2</Button>
+              <Button>Button 3</Button>
+            </ButtonGroup>
+            <ButtonGroup activeIndex={1}>
+              <Button>Button 1</Button>
+              <Button>Button 2</Button>
+              <Button>Button 3</Button>
+            </ButtonGroup>
+            <ButtonGroup>
+              <Button uiRound={true}>Button 1</Button>
+              <Button uiRound={true}>Button 2</Button>
+            </ButtonGroup>
+            <ButtonGroup activeIndex={1}>
+              <Button uiRound={true}>Button 1</Button>
+              <Button uiRound={true}>Button 2</Button>
+            </ButtonGroup>
+            <p><Button uiFill={true}>Fill Button</Button></p>
+            <p><Button uiFill={true} uiStyle="success">Fill Button</Button></p>
+            <p><Button uiFill={true} uiStyle="danger">Fill Button</Button></p>
+            <p><Button uiFill={true} uiStyle="warning">Fill Button</Button></p>
+            <p><Button uiFill={true} style={{ backgroundColor: '#ccc' }}>Custom Style</Button></p>
+          </ContainerBlock>
+        </Container>
+      </div>
     )
   }
 }
--- src/Button.js
+++ src/Button.js
@@ -1,11 +1,73 @@
-import React, { Component } from 'react'
+import React from 'react'
+import classnames from 'classnames'
 
-class Button extends Component {
+class Group extends React.Component {
   render () {
+    const activeIndex = this.props.activeIndex
+    const buttons = React.Children.map(this.props.children, (item, i)=>{
+      return activeIndex == i ? React.cloneElement(item, { active: true }) : item
+    })
     return (
-      <div>{this.props.chilren}</div>
+      <p className="buttons-row">{buttons}</p>
     )
   }
+}
+
+Group.propTypes = {
+  activeIndex: React.PropTypes.number
+}
+
+class Button extends React.Component {
+  render () {
+    let {
+      href,
+      uiStyle,
+      disabled,
+      component: Component,
+      children,
+      ...props
+    } = this.props
+    Component = Component || 'a'
+    href = href || 'javascript:void(0)'
+
+    const classSet = {
+      'button': true
+    }
+    if (uiStyle)
+      classSet['button-' + uiStyle] = true
+    classSet['button-big'] = !!this.props.uiBig
+    classSet['button-round'] = !!this.props.uiRound
+    classSet['button-fill'] = !!this.props.uiFill
+
+    classSet.disabled = !!this.props.disabled
+    classSet.active = !!this.props.active
+
+    const classes = classnames(classSet)
+
+    return (
+      <Component
+        {...props}
+        href={href}
+        className={classes}
+      >
+        {children}
+      </Component>
+    )
+  }
+}
+
+Button.Group = Group
+
+Button.propTypes = {
+  href: React.PropTypes.string,
+  target: React.PropTypes.string,
+  uiStyle: React.PropTypes.string,
+  uiRound: React.PropTypes.bool,
+  uiBig: React.PropTypes.bool,
+  uiFill: React.PropTypes.bool,
+  component: React.PropTypes.node,
+  active: React.PropTypes.bool,
+  disabled: React.PropTypes.bool,
 }
 
 export default Button
