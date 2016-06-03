ea8d0aa2bbfb9b532a1f505cccdb70afe008cc38
Lay <jserlay@gmail.com>
2016-06-03 19:08:05 +0800

    TabBar Update

--- examples/src/views/TabBarView.js
+++ examples/src/views/TabBarView.js
@@ -1,9 +1,33 @@
 import React from 'react'
+import { NavBar, Container, TabBar } from 'layui'
+const ContainerBlock = Container.Block
+const TabBarItem = TabBar.Item
 
 export default class TabBarView extends React.Component {
   render () {
     return (
-      <div></div>
+      <div>
+        <NavBar title="标签页" />
+        <Container>
+          <TabBar defActiveIndex={1}>
+            <TabBarItem title="全部">
+              <ContainerBlock>
+                <p>This is tab 1 content</p>
+              </ContainerBlock>
+            </TabBarItem>
+            <TabBarItem title="待付款">
+              <ContainerBlock>
+                <p>This is tab 2 content</p>
+              </ContainerBlock>
+            </TabBarItem>
+            <TabBarItem title="已付款">
+              <ContainerBlock>
+                <p>This is tab 3 content</p>
+              </ContainerBlock>
+            </TabBarItem>
+          </TabBar>
+        </Container>
+      </div>
     )
   }
 }
--- src/TabBar.js
+++ src/TabBar.js
@@ -1,11 +1,65 @@
 import React, { Component } from 'react'
+import classnames from 'classnames'
 
-class TabBar extends Component {
+class Item extends Component {
   render () {
     return (
       <div></div>
     )
   }
 }
+
+Item.propTypes = {
+  title: React.PropTypes.string
+}
+
+class TabBar extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      activeIndex: this.props.defActiveIndex || 0
+    }
+  }
+
+  handleTabTap(i) {
+    this.setState({ activeIndex: i })
+  }
+
+  render () {
+    const tabLinks = []
+    const tabs = []
+    React.Children.map(this.props.children, (item, i)=>{
+      const tabLinkClass = classnames({
+        'tab-link': true,
+        'active': this.state.activeIndex == i,
+        'button': true
+      })
+      const tabClass = classnames({
+        'tab': true,
+        'active': this.state.activeIndex == i
+      })
+      tabLinks.push(<a key={i} href="javascript:void(0)" className={tabLinkClass} onTouchTap={this.handleTabTap.bind(this, i)}>{item.props.title}</a>)
+      tabs.push(<div className={tabClass} key={i}>{item.props.children}</div>)
+    })
+
+    return (
+      <div>
+        <div className={this.props.uiStyle == 'hollow' ? 'buttons-row' : 'buttons-tab'}>
+          {tabLinks}
+        </div>
+        <div className="tabs">
+          {tabs}
+        </div>
+      </div>
+    )
+  }
+}
+
+TabBar.propTypes = {
+  defActiveIndex: React.PropTypes.number,
+  uiStyle: React.PropTypes.string
+}
+
+TabBar.Item = Item
 
 export default TabBar
