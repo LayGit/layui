4fc2721eb93bd0478d9fda54901aa920de16eeaa
Hansenleee <794704185@qq.com>
2016-06-03 19:06:23 +0800

    ActionSheet Update

--- examples/src/views/ActionSheetView.js
+++ examples/src/views/ActionSheetView.js
@@ -1,9 +1,58 @@
 import React from 'react'
+import { ActionSheet } from 'layui'
 
 export default class ActionSheetView extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      visible:false,
+      onCancel:null
+    }
+  }
+  action1(){
+    this.setState({visible:false})
+    //alert(1)
+  }
+
+  action2(){
+    this.setState({visible:false})
+    //alert(2)
+  }
+
+  showActionSheet(){
+    const state = this.state
+    state.visible = !state.visible
+    state.onCancel = () => {
+      this.setState({visible:false})
+    }
+    this.setState(state)
+  }
+
   render () {
+    const actionList = [
+      {
+        text:'卖出',
+        color:'#ff0000',
+        onTouchTap:() => {
+          this.action1()
+        }
+      },
+      {
+        text:'买入',
+        onTouchTap:() => {
+          this.action2()
+        }
+      }
+    ]
     return (
-      <div></div>
+      <div>
+        <p onTouchTap={this.showActionSheet.bind(this)}>操作</p>
+        <ActionSheet
+          title='请选择'
+          actionList={actionList}
+          visible={this.state.visible}
+          onCancel={this.state.onCancel}/>
+      </div>
     )
   }
 }
--- src/ActionSheet.js
+++ src/ActionSheet.js
@@ -1,11 +1,68 @@
 import React, { Component } from 'react'
+import classnames from 'classnames'
 
 class ActionSheet extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      init:false
+    }
+  }
+  
+  actionList(index){
+    this.props.actionList[index].onTouchTap()
+  }
+
+  onCancel(){
+    const state = this.state
+    if(!state.init){
+      this.setState({init:true})
+    }
+    this.props.onCancel ? this.props.onCancel() : void 0
+  }
   render () {
+    const props = this.props
+    const actionList = props.actionList || []
+    const listHtml = actionList.map((item,index) => {
+      const fontColor = item.color || '#5f646e'
+      return (
+        <span key={index} className='actions-modal-button' style={{color:fontColor}} onTouchTap={this.actionList.bind(this,index)}>{item.text}</span>
+      )
+    })
+    const bodyStyle = this.state.init?{} : {opacity:0}
+    const classModal = props.visible?'modal-overlay  modal-overlay-visible' : 'modal-overlay'
+    const classAction = props.visible?'actions-modal modal-in' : 'actions-modal modal-out'
+    const overlayClass = classnames({
+      'modal-overlay': true,
+      'modal-overlay-visible': props.visible
+    })
+    const modalClass = classnames({
+      'actions-modal': true,
+      'modal-in': props.visible,
+      'modal-out': !props.visible && this.state.init
+    })
     return (
-      <div></div>
+      <div>
+        <div className={overlayClass}></div>
+        <div className={modalClass}>
+          <div className='actions-modal-group'>
+            <span className='actions-modal-label'>{props.title}</span>
+            {listHtml}
+          </div>
+          <div className="actions-modal-group">
+            <span className="actions-modal-button bg-danger" style={{background:'#f6383a',color:'#fff'}} onTouchTap={this.onCancel.bind(this)}>取消</span>
+          </div>
+        </div>
+      </div>
     )
   }
+}
+
+ActionSheet.propTypes = {
+  title: React.PropTypes.string,
+  visible: React.PropTypes.bool,
+  onCancel: React.PropTypes.func,
+  actionList: React.PropTypes.array
 }
 
 export default ActionSheet
