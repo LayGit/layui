03d4f3d92632693c471186b5c8244ca02eb1d140
Lay <jserlay@gmail.com>
2016-06-07 17:37:21 +0800

    ToolBar Updated

--- examples/src/views/ToolBarView.js
+++ examples/src/views/ToolBarView.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import { Container, ToolBar, Icon } from 'layui'
+const ToolBarItem = ToolBar.Item
 
 export default class ToolBarView extends React.Component {
   render () {
     return (
-      <div></div>
+      <div>
+        <Container></Container>
+        <ToolBar activeIndex={0}>
+          <ToolBarItem icon={<Icon name="home" componet="span" />} title="文案"></ToolBarItem>
+          <ToolBarItem icon={<Icon name="me" componet="span" />} title="文案" badge={2}></ToolBarItem>
+          <ToolBarItem icon={<Icon name="star" componet="span" />} title="文案"></ToolBarItem>
+          <ToolBarItem icon={<Icon name="cart" componet="span" />} title="文案"></ToolBarItem>
+          <ToolBarItem icon={<Icon name="settings" componet="span" />} title="文案"></ToolBarItem>
+        </ToolBar>
+      </div>
     )
   }
 }
--- src/ToolBar.js
+++ src/ToolBar.js
@@ -1,11 +1,74 @@
 import React, { Component } from 'react'
+import classnames from 'classnames'
 
-class ToolBar extends Component {
+class Item extends Component {
   render () {
+    let {
+      title,
+      href,
+      linkProps,
+      actived,
+      icon,
+      badge,
+      linkComponent: LinkComponent,
+      ...props
+    } = this.props
+    LinkComponent = LinkComponent || 'a'
+    linkProps = linkProps || {}
+
+    if ('href' in this.props && !linkProps.href)
+      linkProps.href = href
+
+    let textNode = title
+    let badgeNode = null
+    if ('icon' in this.props) {
+      textNode = (<span className="tab-label">{title}</span>)
+      if ('badge' in this.props) {
+        badgeNode = (<span className="badge">{badge}</span>)
+      }
+    }
+
+    let linkComponentClass = {
+      'tab-item': true,
+      'external': true,
+      'active': actived
+    }
+
     return (
-      <div></div>
+      <LinkComponent className={classnames(linkComponentClass)} {...linkProps}>
+        {icon}
+        {textNode}
+        {badgeNode}
+      </LinkComponent>
     )
   }
 }
+
+Item.propTypes = {
+  title: React.PropTypes.string.isRequired,
+  href: React.PropTypes.string,
+  linkProps: React.PropTypes.object,
+  actived: React.PropTypes.bool,
+  icon: React.PropTypes.node,
+  badge: React.PropTypes.number
+}
+
+class ToolBar extends Component {
+  render () {
+    const activeIndex = this.props.activeIndex
+    return (
+      <nav className="bar bar-tab" {...this.props}>
+        {React.Children.map(this.props.children, (item, i)=>{
+          return React.cloneElement(item, {actived: activeIndex == i})
+        })}
+      </nav>
+    )
+  }
+}
+ToolBar.propTypes = {
+  activeIndex: React.PropTypes.number
+}
+
+ToolBar.Item = Item
 
 export default ToolBar
