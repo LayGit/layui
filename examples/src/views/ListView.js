c8a8bdaccd8a26448f85be20491a7d50b95c4a3f
Lay <jserlay@gmail.com>
2016-06-07 16:28:55 +0800

    List Updated

--- examples/src/views/ListView.js
+++ examples/src/views/ListView.js
@@ -1,9 +1,87 @@
 import React from 'react'
+import { NavBar, Container, List } from 'layui'
+const BlockTitle = Container.BlockTitle
+const ListItem = List.Item
+const ListGroup = List.Group
 
 export default class ListView extends React.Component {
   render () {
     return (
-      <div></div>
+      <div>
+        <NavBar title="列表" />
+        <Container>
+          <BlockTitle>图标、标题和副标题</BlockTitle>
+          <List>
+            <ListItem title="商品名称" after="杜蕾斯" />
+            <ListItem title="型号" after="极致超薄型" />
+            <ListItem title="库存" after="123件" />
+          </List>
+          <BlockTitle>标题和副标题</BlockTitle>
+          <List>
+            <ListItem title="商品名称" after="杜蕾斯" />
+            <ListItem title="型号" after="极致超薄型" />
+            <ListItem title="库存" after="123件" />
+          </List>
+          <BlockTitle>带箭头</BlockTitle>
+          <List>
+            <ListItem title="商品名称" after="杜蕾斯" arrow={true} />
+            <ListItem title="型号" after="极致超薄型" arrow={true} />
+            <ListItem title="库存" after="123件" arrow={true} />
+          </List>
+          <BlockTitle>分组</BlockTitle>
+          <List group={true}>
+            <ListGroup title="A">
+              <ListItem title="阿大" />
+              <ListItem title="阿二" />
+              <ListItem title="阿三" />
+            </ListGroup>
+            <ListGroup title="B">
+              <ListItem title="本大" />
+              <ListItem title="本二" />
+              <ListItem title="本三" />
+            </ListGroup>
+          </List>
+          <BlockTitle>曲目</BlockTitle>
+          <List listStyle="media">
+            <ListItem
+              media={<img src="http://gqianniu.alicdn.com/bao/uploaded/i4//tfscom/i3/TB10LfcHFXXXXXKXpXXXXXXXXXX_!!0-item_pic.jpg_250x250q60.jpg" style={{ width: '4rem' }} />}
+              title="标题"
+              after="$15"
+              subTitle="副标题"
+              text="此处是文本内容..."
+              arrow={true}
+              href="#"
+            />
+          </List>
+          <BlockTitle>邮件应用</BlockTitle>
+          <List listStyle="media">
+            <ListItem
+              title="Facebook"
+              after="17:14"
+              subTitle="子标题"
+              text="此处是文本内容..."
+              href="#"
+            />
+          </List>
+          <BlockTitle>简单列表</BlockTitle>
+          <List listStyle="media">
+            <ListItem
+              media={<img src="http://gqianniu.alicdn.com/bao/uploaded/i4//tfscom/i3/TB10LfcHFXXXXXKXpXXXXXXXXXX_!!0-item_pic.jpg_250x250q60.jpg" style={{ width: '2.2rem' }} />}
+              title="标题"
+              subTitle="子标题"
+            />
+          </List>
+          <BlockTitle>嵌入式</BlockTitle>
+          <List listStyle="media" inset={true}>
+            <ListItem
+              media={<img src="http://gqianniu.alicdn.com/bao/uploaded/i4//tfscom/i3/TB10LfcHFXXXXXKXpXXXXXXXXXX_!!0-item_pic.jpg_250x250q60.jpg" style={{ width: '2.2rem' }} />}
+              title="标题"
+              subTitle="子标题"
+              href="http://www.baidu.com"
+            />
+          </List>
+        </Container>
+      </div>
     )
   }
 }
--- src/List.js
+++ src/List.js
@@ -1,11 +1,136 @@
 import React, { Component } from 'react'
+import classnames from 'classnames'
 
-class List extends Component {
+class Group extends Component {
   render () {
     return (
-      <div></div>
+      <div className="list-group">
+        <ul>
+          <li className="list-group-title">{this.props.title}</li>
+          {this.props.children}
+        </ul>
+      </div>
     )
   }
 }
+
+Group.propTypes = {
+  title: React.PropTypes.string.isRequired
+}
+
+class Item extends Component {
+  render () {
+    let {
+      media,
+      title,
+      after,
+      subTitle,
+      text,
+      arrow,
+      href,
+      linkProps,
+      linkComponent: LinkComponent,
+      ...props
+    } = this.props
+
+    let hasLink = false
+    linkProps = linkProps || {}
+
+    if (!('linkComponent' in this.props))
+      LinkComponent = 'div'
+
+    if ('href' in this.props) {
+      LinkComponent = 'a'
+      linkProps.href = href
+      hasLink = true
+    }
+
+    if ('linkProps' in this.props)
+      hasLink = true
+
+    let liClass = {
+      'item-content': true,
+      'item-link': arrow || hasLink
+    }
+
+    let mediaNode = null
+    if ('media' in this.props)
+      mediaNode = (<div className="item-media">{media}</div>)
+    let titleNode = (<div className="item-title">{title}</div>)
+
+    let afterNode = null
+    if ('after' in this.props) {
+      afterNode = (<div className="item-after">{after}</div>)
+    }
+
+    let hasSubTitleOrText = false
+
+    let subTitleNode = null
+    if ('subTitle' in this.props) {
+      hasSubTitleOrText = true
+      subTitleNode = (<div className="item-subtitle">{subTitle}</div>)
+    }
+
+    let textNode = null
+    if ('text' in this.props) {
+      hasSubTitleOrText = true
+      textNode = (<div className="item-text">{text}</div>)
+    }
+
+    let innerNode = (<div className="item-inner">{titleNode}{afterNode}</div>)
+    if (hasSubTitleOrText)
+      innerNode = (<div className="item-inner"><div className="item-title-row">{titleNode}{afterNode}</div>{subTitleNode}{textNode}</div>)
+    return (
+      <li {...props}>
+        <LinkComponent className={classnames(liClass)} {...linkProps}>
+          {mediaNode}
+          {innerNode}
+        </LinkComponent>
+      </li>
+    )
+  }
+}
+
+Item.propTypes = {
+  media: React.PropTypes.node,
+  title: React.PropTypes.string.isRequired,
+  after: React.PropTypes.oneOfType([React.PropTypes.string, React.PropTypes.node]),
+  subTitle: React.PropTypes.string,
+  text: React.PropTypes.string,
+  arrow: React.PropTypes.bool,
+  href: React.PropTypes.string,
+  linkProps: React.PropTypes.object
+}
+
+class List extends Component {
+  render () {
+    let {
+      group,
+      inset,
+      listStyle,
+      children,
+      ...props
+    } = this.props
+    let listClass = {
+      'list-block': true,
+      'media-list': listStyle == 'media',
+      'inset': inset
+    }
+    return (
+      <div className={classnames(listClass)} {...props}>
+        {group ? children : (<ul>{children}</ul>)}
+      </div>
+    )
+  }
+}
+
+List.propTypes = {
+  group: React.PropTypes.bool,
+  inset: React.PropTypes.bool,
+  listStyle: React.PropTypes.string
+}
+
+List.Item = Item
+List.Group = Group
 
 export default List
