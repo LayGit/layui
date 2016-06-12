ed4641ceb29ec127fdf2d1cd7267afc5994571f3
Lay <jserlay@gmail.com>
2016-06-12 16:42:52 +0800

    Grid Updated

--- src/Grid.js
+++ src/Grid.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react'
 
-class Grid extends Component {
+class Col extends Component {
   render () {
+    const colClass = 'col-' + this.props.span
     return (
-      <div></div>
+      <div className={colClass}>{this.props.children}</div>
     )
   }
 }
+
+class Grid extends Component {
+  render () {
+    return (
+      <div className="row">
+        {this.props.children}
+      </div>
+    )
+  }
+}
+
+Grid.Col = Col
 
 export default Grid
