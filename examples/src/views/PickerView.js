36e518c8705e6ea8b2bbd95b62f58cc1162b7e1f
Hansenleee <794704185@qq.com>
2016-06-13 17:53:57 +0800

    picker submit

--- examples/src/views/PickerView.js
+++ examples/src/views/PickerView.js
@@ -1,9 +1,111 @@
 import React from 'react'
+import { NavBar, Container, picker } from 'layui'
 
 export default class PickerView extends React.Component {
+  pickerHandle(e){
+    const datas = [{
+      text: 'iphone4',
+      value: 4
+    },{
+      text: 'iphone5',
+      value: 5
+    },{
+      text: 'iphone6',
+      value: 6
+    },{
+      text: '小米1',
+      value: 7
+    },{
+      text: '小米2',
+      value: 8
+    },{
+      text: '小米3',
+      value: 9
+    }]
+
+    picker.picker({
+      title: '请选择手机',
+      content: [{
+        col: '手机型号',
+        values: datas
+      }]
+    },e.target,(obj) => {
+      console.log(obj)
+    })
+  }
+
+  pickerNameHandle(e){
+    const datas1 = [
+      {
+        text: '赵',
+        value: 0
+      },
+      {
+        text: '钱',
+        value: 1
+      },{
+        text: '孙',
+        value: 2
+      },{
+        text: '李',
+        value: 3
+      }
+    ]
+    const datas2 = [
+      {
+        text: '小三',
+        value: 3
+      },{
+        text: '小四',
+        value: 4
+      },{
+        text: '小五',
+        value: 5
+      },{
+        text: '小六',
+        value: 6
+      }
+    ]
+    const datas3 = [
+      {
+        text: '先生',
+        value: 1
+      },{
+        text: '小姐',
+        value: 2
+      }
+    ]
+    picker.picker({
+      title: '请选择手机',
+      content: [{
+        col: '姓',
+        values: datas1
+      },{
+        col: '名',
+        values: datas2
+      },{
+        col: '称呼',
+        values: datas3
+      }]
+    },e.target,(obj) => {
+      console.log(obj)
+    })
+  }
   render () {
     return (
-      <div></div>
+      <div>
+        <NavBar title="picker" />
+        <Container>
+          <div style={{margin:20}}>
+            <span>手机</span>
+            <input placeholder='手机' onClick={this.pickerHandle.bind(this)} readOnly></input>
+          </div>
+          <div style={{margin:20}}>
+            <span>称呼</span>
+            <input placeholder='称呼' onClick={this.pickerNameHandle.bind(this)} readOnly></input>
+          </div>
+        </Container>
+      </div>
     )
   }
 }
--- src/Picker.js
+++ src/Picker.js
@@ -1,11 +1,242 @@
 import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import classnames from 'classnames'
 
 class Picker extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      visible: false,
+      onMove: [],
+      touch: {
+        flag: false,
+        y: null
+      }
+    }
+  }
+
+  componentDidMount(){
+    //弹出效果
+    setTimeout(() => {
+      this.setState({ visible:true })
+    },0)
+    //点击关闭
+    window.addEventListener('click',this.pickerOnBlur.bind(this),false)
+    //初始值
+    const state = this.state
+    const length = this.props.content.length
+    for(var i = 0;i < length;i ++){
+      let index = this.props.target.getAttribute('data-selected' + i)
+      if(!index){
+        index = 0
+      }
+      state.onMove.push({
+        onMoves: 90 - index * 36,
+        transitionTime: '300ms',
+        selected: index
+      })
+    }
+    this.setState(state)
+    this.valueInputHandle()
+  }
+
+  pickerOnBlur(e){
+    const state = this.state
+    if(!state.visible){
+      return
+    }
+    let target = e.target
+    while (target.tagName != 'BODY') {
+      if(target == this.props.target || (target.className || '').indexOf('picker-columns') > -1){
+        return
+      }else{
+        target = target.parentNode
+      }
+    }
+    //关闭
+    this.onOkHandle()
+  }
+
+  onTouchStartHandle(index,e){
+    const state = this.state
+    state.onMove[index].transitionTime = '0ms'
+    state.touch = {
+      flag: true,
+      y: e.touches[0].clientY
+    }
+    this.setState(state)
+  }
+
+  onTouchMoveHandle(index,e){
+    const state = this.state
+    if(!state.touch.flag){
+      return
+    }
+    const touches = e.touches[0]
+    const dis = touches.clientY - state.touch.y
+    state.onMove[index].onMoves += dis
+    state.touch.y = touches.clientY
+    //拉动范围
+    const onMovesDis = state.onMove[index].onMoves
+    const maxDis = 90 - this.props.content[index].values.length * 36
+    if(onMovesDis > 126){
+      state.onMove[index].onMoves = 126
+    }else if (onMovesDis < maxDis) {
+      state.onMove[index].onMoves = maxDis
+    }
+    //滑动高亮显示
+    let _index = -Math.round((onMovesDis - 90) / 36)
+    const maxLength = this.props.content[index].values.length
+    if(_index < 0){
+      _index = 0
+    }else if(_index >= maxLength){
+      _index = maxLength - 1
+    }
+    state.onMove[index].selected = _index
+    this.setState(state)
+    this.valueInputHandle()
+  }
+
+  onTouchEndHandle(index,e){
+    const state = this.state
+    state.onMove[index].transitionTime = '300ms'
+    state.touch.flag = false
+    this.setState(state)
+    //高亮显示的内容归位
+    setTimeout(() => {
+      const state = this.state
+      for(var i = 0;i < state.onMove.length;i ++){
+        const item = state.onMove[i]
+        item.onMoves = 90 - 36 * item.selected
+      }
+      this.setState(state)
+    },0)
+  }
+
+  valueInputHandle(){
+    let value = ''
+    const state = this.state
+    for(var i = 0;i < state.onMove.length;i ++){
+      value += this.props.content[i].values[state.onMove[i].selected].text + ' '
+    }
+    this.props.target.value = value
+  }
+
+  onOkHandle(){
+    const state = this.state
+    state.visible = false
+    this.setState(state)
+    setTimeout(() => {
+      this.props.close()
+    },400)
+    //关闭事件
+    window.removeEventListener('click',this.pickerOnBlur.bind(this),false)
+    //回调
+    let ret = []
+    for(var i = 0;i < state.onMove.length;i ++){
+      this.props.target.setAttribute('data-selected' + i,state.onMove[i].selected)
+      ret.push({
+        col: this.props.content[i].col,
+        value: this.props.content[i].values[state.onMove[i].selected]
+      })
+    }
+    this.props.callBack(ret)
+  }
+
   render () {
+    const state = this.state
+    const props = this.props
+    const className = classnames({
+      'picker-modal picker-columns  remove-on-close': true,
+      'modal-in': state.visible,
+      'modal-out': !state.visible
+    })
+    const html = this.props.content.map((item,index) => {
+      const flag = state.onMove.length > index
+      const onMoves = flag ? state.onMove[index].onMoves : 90
+      const transitionTime = flag ? state.onMove[index].transitionTime : '300ms'
+      const selected = flag ? state.onMove[index].selected : 0
+      const innerHtml = (item.values || []).map((item,_index) => {
+        let className = 'picker-item'
+        if(_index == selected){
+          className = 'picker-item picker-selected'
+        }
+        return (
+          <div key={_index} className={className}>{item.text}</div>
+        )
+      })
+      return (
+        <div key={index} className='picker-items-col picker-items-col-center'>
+          <div
+            className='picker-items-col-wrapper'
+            style={{transform: 'translate3d(0px, ' + onMoves + 'px, 0px)',transition:transitionTime}}
+            onTouchStart={this.onTouchStartHandle.bind(this,index)}
+            onTouchMove={this.onTouchMoveHandle.bind(this,index)}
+            onTouchEnd={this.onTouchEndHandle.bind(this,index)}>
+            {innerHtml}
+          </div>
+        </div>
+      )
+    })
     return (
-      <div></div>
+      <div>
+        <div className={className}>
+          <header className="bar bar-nav">
+            <button className="button button-link pull-right close-picker" style={{zIndex:9}} onTouchTap={this.onOkHandle.bind(this)}>确定</button>
+            <h1 className="title">{props.title}</h1>
+          </header>
+          <div className='picker-modal-inner picker-items'>
+            {html}
+            <div className="picker-center-highlight"></div>
+          </div>
+        </div>
+      </div>
     )
   }
 }
 
-export default Picker
+let globalTarget = []
+
+function picker(config,target,callBack){
+  if(!findExist(target)){
+    return
+  }
+  globalTarget.push(target)
+  let div = document.createElement('div')
+  div.id = 'react-picker'
+  let handleClose = function(){
+    if(!div){
+      return
+    }
+    deleteExist(target)
+    ReactDOM.unmountComponentAtNode(div)
+    document.body.removeChild(div)
+  }
+  document.body.appendChild(div)
+  let array = config.content
+  if(!array instanceof Array){
+    config.content = new Array[array]
+  }
+  ReactDOM.render(<Picker {...config} target={target} callBack={callBack} close={handleClose}/>,div)
+}
+
+function findExist(target){
+  for(var i = 0;i < globalTarget.length;i ++){
+    if(globalTarget[i] == target){
+      return false
+    }
+  }
+  return true
+}
+
+function deleteExist(target){
+  for(var i = 0;i < globalTarget.length;i ++){
+    if(globalTarget[i] == target){
+      globalTarget.splice(i,1)
+    }
+  }
+}
+
+export default {
+  picker: picker
+}
