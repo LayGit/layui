73c4c374b2184c8739352816eb3a6bd9c7277fcd
Hansenleee <794704185@qq.com>
2016-06-13 12:28:38 +0800

    calendar update

--- examples/src/views/CalendarView.js
+++ examples/src/views/CalendarView.js
@@ -2,10 +2,10 @@ import React from 'react'
 import { NavBar, Container, calendar } from 'layui'
 
 export default class CalendarView extends React.Component {
-  calendarHandle(){
+  calendarHandle(e){
     const date = new Date()
-    calendar.showCalendar(date, (obj)=>{
-
+    calendar.showCalendar(date, e.target, (obj)=>{
+      console.log(obj)
     })
   }
   render () {
@@ -14,7 +14,7 @@ export default class CalendarView extends React.Component {
         <NavBar title="日历" />
         <Container>
           <span>日期：</span>
-          <input placeholder='请选择日期' onFocus={this.calendarHandle.bind(this)}/>
+          <input placeholder='请选择日期' onClick={this.calendarHandle.bind(this)} readOnly></input>
         </Container>
       </div>
     )
--- src/Calendar.js
+++ src/Calendar.js
@@ -7,8 +7,10 @@ class DateMonthContent extends Component{
     if(month == 12){
       year ++
       month = 1
+    }else{
+      month ++
     }
-    const nextMonthFirstDay = new Date(year + '-' + month + '-1')
+    const nextMonthFirstDay = new Date(year + '/' + month + '/1')
     const days = new Date(nextMonthFirstDay.getTime() - 1000 * 24 * 60 * 60).getDate()
     return days
   }
@@ -27,6 +29,26 @@ class DateMonthContent extends Component{
     return day == 0 ? 7 : day
   }
 
+  getXMoves(){
+    let moves = - parseFloat(this.props.moves)
+    const oldMoves = - this.props.oldMoves
+    if(moves % 100 != 0){
+      moves = oldMoves
+    }
+    switch (this.props.position) {
+      case -1:
+        return -100 + moves + '%'
+      case 0:
+        return moves + '%'
+      case 1:
+        return 100 + moves + '%'
+    }
+  }
+
+  dateSelectHandle(item){
+    this.props.onSelect(this.props.year,item.month,item.value)
+  }
+
   render(){
     const props = this.props
     const calendarClass = classnames({
@@ -35,6 +57,7 @@ class DateMonthContent extends Component{
       'picker-calendar-month-current': props.position == 0,
       'picker-calendar-month-next': props.position == 1
     })
+    const xMoves = this.getXMoves()
     const year = props.year
     const month = props.month
     //本月第一天，最后一天，本月长度
@@ -50,26 +73,37 @@ class DateMonthContent extends Component{
         var index = (row - 1) * 7 + i
         //判断是否是这个月内的，
         if(index < firstDayWeek){
+          var valueMonth = month - 1
           var valueDay = lastMonthDays + index - firstDayWeek
           var className = 'picker-calendar-day picker-calendar-day-prev picker-calendar-day-weekend'
-        }else if(index > days){
+        }else if(index > days + firstDayWeek - 1){
+          var valueMonth = month + 1
           var valueDay = index - days
-          var className = 'picker-calendar-day picker-calendar-day-prev picker-calendar-day-weekend'
+          var className = 'picker-calendar-day picker-calendar-day-next picker-calendar-day-weekend'
         }else{
+          var valueMonth = month
           var valueDay = index - firstDayWeek + 1
           var className = 'picker-calendar-day'
         }
         list.push({
           value: valueDay,
-          className: className
+          className: className,
+          month: valueMonth
         })
       }
       htmlList.push(list)
     }
     const html = htmlList.map((item,index) => {
       const everyHtml = item.map((item,index) => {
+        const sYear = props.select.getFullYear()
+        const sMonth = props.select.getMonth() + 1
+        const day = props.select.getDate()
+        let className = item.className
+        if(sYear == props.year && day == item.value && item.month == sMonth){
+          className += ' picker-calendar-day-selected'
+        }
         return (
-          <div key={index} className={item.className}>
+          <div key={index} className={className} onTouchTap={this.dateSelectHandle.bind(this,item)}>
             <span>{item.value}</span>
           </div>
         )
@@ -81,7 +115,7 @@ class DateMonthContent extends Component{
       )
     })
     return(
-      <div className={calendarClass}>
+      <div className={calendarClass} style={{transform:'translate3d(' + xMoves + ', 0%, 0px)'}}>
         {html}
       </div>
     )
@@ -95,7 +129,16 @@ class Calendar extends Component {
       initFlag: false,
       visible: false,
       year: '',
-      month: ''
+      month: '',
+      select: new Date(),
+      xMoves:'0%',
+      xMovesOld:'0%',
+      transitionTime:'300ms',
+      touchMove: {
+        flag:false,
+        x:null,
+        y:null
+      }
     }
   }
 
@@ -105,6 +148,7 @@ class Calendar extends Component {
     const date = this.props.default
     state.year = date.getFullYear()
     state.month = date.getMonth()
+    state.select = date
     this.setState(state)
     //关闭日历
     window.addEventListener('click',this.calendarOnBlur.bind(this),false)
@@ -113,7 +157,7 @@ class Calendar extends Component {
       this.setState({ initFlag : true, visible : true})
     },0)
     //回调事件
-    this.props.callBack(this.props.date)
+    this.props.callBack(this.props.default)
   }
 
   calendarOnBlur(e){
@@ -123,14 +167,18 @@ class Calendar extends Component {
     }
     let target = (e || window.event).target
     while (target.tagName != 'BODY') {
-      if((target.className || '').indexOf('picker-modal') > -1){
+      if((target.className || '').indexOf('picker-modal') > -1 || target == this.props.target){
         return
       }else{
         target = target.parentNode
       }
     }
     this.setState({ visible : false })
-    const time = setTimeout(closeCalendarComponent,400)
+    setTimeout(() => {
+      this.props.closeFn()
+    }.bind(this),400)
+    //关闭事件
+    window.removeEventListener('click',this.calendarOnBlur.bind(this),false)
   }
 
   getMonthInChinese(month){
@@ -140,43 +188,121 @@ class Calendar extends Component {
 
   monthPrev(){
     const state = this.state
+    const moves = this.getXmovesInteger()
+    state.xMoves = moves + 100 + '%'
     if(state.month == 0){
       state.year --
       state.month = 11
     }else{
       state.month --
     }
+    state.xMovesOld = state.xMoves
     this.setState(state)
   }
 
   monthNext(){
     const state = this.state
+    const moves = this.getXmovesInteger()
+    state.xMoves = moves - 100 + '%'
     if(state.month == 11){
       state.year ++
       state.month = 0
     }else{
       state.month ++
     }
+    state.xMovesOld = state.xMoves
     this.setState(state)
   }
 
   yearPrev(){
     const state = this.state
+    const moves = this.getXmovesInteger()
+    state.xMoves = moves + 100 + '%'
     state.year --
+    state.xMovesOld = state.xMoves
     this.setState(state)
   }
 
   yearNext(){
     const state = this.state
+    const moves = this.getXmovesInteger()
+    state.xMoves = moves - 100 + '%'
     state.year ++
+    state.xMovesOld = state.xMoves
     this.setState(state)
+  }
+
+  onTouchStartHandle(e){
+    e.preventDefault()
+    const state = this.state
+    state.touchMove = {
+      flag : true,
+      x : e.touches[0].clientX,
+      y : e.touches[0].clientY
+    }
+    state.xMovesOld = state.xMoves
+    this.setState(state)
+  }
+
+  getXmovesInteger(){
+    return parseFloat(this.state.xMovesOld)
+  }
+
+  onTouchMoveHandle(e){
+    e.preventDefault()
+    const state = this.state
+    if(!state.touchMove.flag){
+      return
+    }
+    const touches = e.touches[0]
+    const xValues = touches.clientX - state.touchMove.x
+    const width = document.body.scrollWidth
+    const percent = ( xValues / width ) * 100
+    if(xValues != 0){
+      state.xMoves = parseFloat(state.xMovesOld) + percent + '%'
+      state.transitionTime = '0ms'
+    }
+    this.setState(state)
+  }
+
+  onTouchEndHandle(e){
+    e.preventDefault()
+    const state = this.state
+    state.touchMove.flag = false
+    //判断移动后续处理页面
+    const xMoves = parseFloat(state.xMoves) - parseFloat(state.xMovesOld)
+    if(xMoves > 50){
+      this.monthPrev()
+    }else if(xMoves < -50){
+      this.monthNext()
+    }else{
+      state.xMoves = state.xMovesOld
+    }
+    state.transitionTime = '300ms'
+    this.setState(state)
+  }
+
+  dateSelect(year,month,day){
+    const state = this.state
+    const date = new Date(year + '/' + month + '/' + day)
+    state.select = date
+    state.visible = false
+    this.setState(state)
+    this.props.target.value = year + '-' + month + '-' + day
+    setTimeout(() => {
+      this.props.closeFn()
+    }.bind(this),400)
+    //关闭事件
+    window.removeEventListener('click',this.calendarOnBlur.bind(this),false)
+    //回调
+    this.props.callBack(this.state.select)
   }
 
   render () {
     const modalClass = classnames({
       'picker-modal picker-calendar  remove-on-close' : true,
       'modal-in' : this.state.initFlag && this.state.visible,
-      'modal-out' : !this.state.visible
+      'modal-out' : this.state.initFlag && !this.state.visible
     })
     return (
       <div className={modalClass}>
@@ -212,11 +338,11 @@ class Calendar extends Component {
             <div className="picker-calendar-week-day picker-calendar-week-day-weekend"> 周六</div>
             <div className="picker-calendar-week-day picker-calendar-week-day-weekend"> 周日</div>
           </div>
-          <div className='picker-calendar-months'>
-            <div className='picker-calendar-months-wrapper'>
-              <DateMonthContent year={this.state.year} month={this.state.month} position={-1}/>
-              <DateMonthContent year={this.state.year} month={this.state.month + 1} position={0}/>
-              <DateMonthContent year={this.state.year} month={this.state.month + 2} position={1}/>
+          <div className='picker-calendar-months' onTouchStart={this.onTouchStartHandle.bind(this)} onTouchMove={this.onTouchMoveHandle.bind(this)} onTouchEnd={this.onTouchEndHandle.bind(this)}>
+            <div className='picker-calendar-months-wrapper' style={{transform:'translate3d(' + this.state.xMoves + ', 0%, 0px)',transitionDuration:this.state.transitionTime}}>
+              <DateMonthContent year={this.state.year} month={this.state.month} select={this.state.select} position={-1} moves={this.state.xMoves} oldMoves={this.state.xMovesOld} onSelect={this.dateSelect.bind(this)}/>
+              <DateMonthContent year={this.state.year} month={this.state.month + 1} select={this.state.select} position={0} moves={this.state.xMoves} oldMoves={this.state.xMovesOld} onSelect={this.dateSelect.bind(this)}/>
+              <DateMonthContent year={this.state.year} month={this.state.month + 2} select={this.state.select} position={1} moves={this.state.xMoves} oldMoves={this.state.xMovesOld} onSelect={this.dateSelect.bind(this)}/>
             </div>
           </div>
         </div>
@@ -225,32 +351,31 @@ class Calendar extends Component {
   }
 }
 
-function showCalendarComponent(date,callBack){
+function showCalendarComponent(date,target,callBack){
   if(document.getElementById('react-calendar')){
     return
   }
   let div = document.createElement('div')
   div.id = 'react-calendar'
-  document.body.appendChild(div)
-  ReactDOM.render(<Calendar default={new Date()} callBack={callBack}/>,div)
-}
-
-function closeCalendarComponent(){
-  let div = document.getElementById('react-calendar')
-  if(!div){
-    return
+  let handleClose = function(){
+    if(!div){
+      return
+    }
+    ReactDOM.unmountComponentAtNode(div)
+    document.body.removeChild(div)
   }
-  document.body.removeChild(div)
+  document.body.appendChild(div)
+  ReactDOM.render(<Calendar default={date} callBack={callBack} target={target} closeFn={handleClose}/>,div)
 }
 
 const calendar = {
-  showCalendar(date = new Date(),callBack){
-    if(date instanceof Date){
-      showCalendarComponent(date,callBack)
+  showCalendar(date = new Date(),target,callBack){
+    if(target.value){
+      date = new Date((target.value).replace(/-/g,'/'))
     }
-  },
-  closeCalendar(){
-    closeCalendarComponent()
+    if(date instanceof Date){
+      showCalendarComponent(date,target,callBack)
+    }
   }
 }
 
